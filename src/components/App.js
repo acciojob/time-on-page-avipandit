5a6fbff57b2a4fc9983ea091e4348e7d2654a4e1
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-05-19 22:02:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
 
-const App = () => {
+  const handleOnMount = () => {
+    setInterval(() => {
+      setSeconds(seconds + 1);
+    }, 1000);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default Timer;
